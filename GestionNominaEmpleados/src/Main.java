a61bc32168204a4221cee78c23e2be71c9c5463b
jhzz-8 <juanj.juradoc@uqvirtual.edu.co>
2026-04-07 17:03:55 -0500

    Update Main.java

--- GestionNominaEmpleados/src/Main.java
+++ GestionNominaEmpleados/src/Main.java
@@ -1,13 +1,85 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Empresa empresa = new Empresa();
+
+        int opcion;
+
+        do {
+            System.out.println("\n--- MENÚ ---");
+            System.out.println("1. Agregar empleado planta");
+            System.out.println("2. Agregar empleado ventas");
+            System.out.println("3. Agregar empleado temporal");
+            System.out.println("4. Mostrar empleados");
+            System.out.println("5. Buscar por documento");
+            System.out.println("6. Mayor salario");
+            System.out.println("7. Nómina total");
+            System.out.println("8. Resumen pagos");
+            System.out.println("9. Salir");
+
+            opcion = sc.nextInt();
+
+            try {
+                switch (opcion) {
+
+                    case 1 -> {
+                        Empleado e = new EmpleadoPlanta(
+                                "Juan", "123", 25,
+                                2000000, CategoriaEmpleado.SENIOR,
+                                4, 4,
+                                "Ingeniero", 10, 20000, 140000
+                        );
+                        empresa.agregarEmpleado(e);
+                    }
+
+                    case 2 -> {
+                        Empleado e = new EmpleadoVentas(
+                                "Ana", "456", 30,
+                                1500000, CategoriaEmpleado.SEMI_SENIOR,
+                                4, 4,
+                                5000000, 5
+                        );
+                        empresa.agregarEmpleado(e);
+                    }
+
+                    case 3 -> {
+                        Empleado e = new EmpleadoTemporal(
+                                "Luis", "789", 22,
+                                0, CategoriaEmpleado.JUNIOR,
+                                4, 4,
+                                20, 50000
+                        );
+                        empresa.agregarEmpleado(e);
+                    }
+
+                    case 4 -> empresa.mostrarEmpleados();
+
+                    case 5 -> {
+                        System.out.println("Documento:");
+                        String doc = sc.next();
+                        Empleado e = empresa.buscarPorDocumento(doc);
+                        if (e != null) e.mostrarInformacion();
+                        else System.out.println("No encontrado");
+                    }
+
+                    case 6 -> {
+                        Empleado e = empresa.empleadoMayorSalario();
+                        if (e != null) e.mostrarInformacion();
+                    }
+
+                    case 7 -> System.out.println("Total nómina: " + empresa.nominaTotal());
+
+                    case 8 -> empresa.mostrarResumenes();
+
+                }
+
+            } catch (Exception e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+
+        } while (opcion != 9);
     }
 }
