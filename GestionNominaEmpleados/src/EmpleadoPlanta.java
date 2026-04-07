f314546488f893a2d703286252320e26bb2f920d
cristianriosb-web <cristian.riosb@uqvirtual.edu.co>
2026-04-07 17:22:01 -0500

    Update EmpleadoPlanta.java

--- GestionNominaEmpleados/src/EmpleadoPlanta.java
+++ GestionNominaEmpleados/src/EmpleadoPlanta.java
@@ -1,32 +1,21 @@
 public class EmpleadoPlanta extends Empleado {
-
     private String cargo;
     private int horasExtra;
     private float valorHoraExtra;
     private float auxilioTransporte;
 
-    public EmpleadoPlanta(String nombre, String documento, int edad,
-                          float salarioBase, CategoriaEmpleado categoria,
-                          float descuentoSalud, float descuentoPension,
-                          String cargo, int horasExtra,
-                          float valorHoraExtra, float auxilioTransporte) {
-
-        super(nombre, documento, edad, salarioBase, categoria, descuentoSalud, descuentoPension);
-
-        if (horasExtra < 0) throw new IllegalArgumentException("Horas extra inválidas");
-        if (valorHoraExtra < 0) throw new IllegalArgumentException("Valor hora inválido");
-
+    public EmpleadoPlanta(String nombre, String documento, int edad, float salarioBase, 
+                          CategoriaEmpleado cat, float dS, float dP, 
+                          String cargo, int hE, float vHE, float auxT) {
+        super(nombre, documento, edad, salarioBase, cat, dS, dP);
         this.cargo = cargo;
-        this.horasExtra = horasExtra;
-        this.valorHoraExtra = valorHoraExtra;
-        this.auxilioTransporte = auxilioTransporte;
+        this.horasExtra = (hE < 0) ? 0 : hE;
+        this.valorHoraExtra = (vHE < 0) ? 0 : vHE;
+        this.auxilioTransporte = auxT;
     }
 
     @Override
     public float calcularSalarioBruto() {
-        return salarioBase
-                + calcularBonificacionCategoria()
-                + (horasExtra * valorHoraExtra)
-                + auxilioTransporte;
+        return salarioBase + calcularBonificacionCategoria() + (horasExtra * valorHoraExtra) + auxilioTransporte;
     }
 }
