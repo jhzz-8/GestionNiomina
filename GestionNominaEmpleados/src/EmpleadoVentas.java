671028e19bef0ed8e4cc440ff2c87f8c7b9fe850
cristianriosb-web <cristian.riosb@uqvirtual.edu.co>
2026-04-07 17:22:33 -0500

    Update EmpleadoVentas.java

--- GestionNominaEmpleados/src/EmpleadoVentas.java
+++ GestionNominaEmpleados/src/EmpleadoVentas.java
@@ -3,23 +3,15 @@ public class EmpleadoVentas extends Empleado {
     private float porcentajeComision;
 
     public EmpleadoVentas(String nombre, String documento, int edad, float salarioBase, 
-                          CategoriaEmpleado categoria, float dSalud, float dPension,
-                          float totalVentas, float porcentajeComision) {
-        super(nombre, documento, edad, salarioBase, categoria, dSalud, dPension);
-        
-        this.totalVentas = totalVentas;
-
-        if (porcentajeComision < 0 || porcentajeComision > 100) {
-            System.out.println("Error: Comisión fuera de rango. Se asignará 0%.");
-            this.porcentajeComision = 0;
-        } else {
-            this.porcentajeComision = porcentajeComision;
-        }
+                          CategoriaEmpleado cat, float dS, float dP, 
+                          float tVentas, float pComision) {
+        super(nombre, documento, edad, salarioBase, cat, dS, dP);
+        this.totalVentas = tVentas;
+        this.porcentajeComision = (pComision < 0 || pComision > 100) ? 0 : pComision;
     }
 
     @Override
-    public float calcularSalarioBruto() { 
-        float comision = totalVentas * (porcentajeComision / 100); 
-        return salarioBase + calcularBonificacionCategoria() + comision;
+    public float calcularSalarioBruto() {
+        return salarioBase + calcularBonificacionCategoria() + (totalVentas * porcentajeComision / 100);
     }
 }
