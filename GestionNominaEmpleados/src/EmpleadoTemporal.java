f1cd93185856052b55683d56f8f0ebea814b18e5
jhzz-8 <juanj.juradoc@uqvirtual.edu.co>
2026-04-07 17:01:23 -0500

    Implement EmpleadoTemporal class with salary calculation

--- GestionNominaEmpleados/src/EmpleadoTemporal.java
+++ GestionNominaEmpleados/src/EmpleadoTemporal.java
@@ -1,2 +1,25 @@
-public class EmpleadoTemporal {
+public class EmpleadoTemporal extends Empleado {
+
+    private int diasTrabajados;
+    private float valorDia;
+
+    public EmpleadoTemporal(String nombre, String documento, int edad,
+                            float salarioBase, CategoriaEmpleado categoria,
+                            float descuentoSalud, float descuentoPension,
+                            int diasTrabajados, float valorDia) {
+
+        super(nombre, documento, edad, salarioBase, categoria, descuentoSalud, descuentoPension);
+
+        if (diasTrabajados < 0) throw new IllegalArgumentException("Días inválidos");
+        if (valorDia < 0) throw new IllegalArgumentException("Valor día inválido");
+
+        this.diasTrabajados = diasTrabajados;
+        this.valorDia = valorDia;
+    }
+
+    @Override
+    public float calcularSalarioBruto() {
+        float pago = diasTrabajados * valorDia;
+        return pago + calcularBonificacionCategoria();
+    }
 }
