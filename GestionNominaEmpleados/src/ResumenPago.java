e685c966fb2600aeca75efa555204cdc134919a8
cristianriosb-web <cristian.riosb@uqvirtual.edu.co>
2026-04-07 17:53:57 -0500

    Update ResumenPago.java

--- GestionNominaEmpleados/src/ResumenPago.java
+++ GestionNominaEmpleados/src/ResumenPago.java
@@ -1,9 +1,12 @@
-public record ResumenPago{
-    String documento;
-    String nombre;
-    String tipoEmpleado;
-    float salarioBruto;
-    float descuentos;
-    float salarioNet;
+public record ResumenPago() {
+    public static String documento;
+    public static String nombre;
+    public static String tipoEmpleado;
+    public static float salarioBruto;
+    public static float descuentos;
+    public static float salarioNet;
 
+    public ResumenPago(String documento, String nombre, String simpleName, float v, float v1, float v2) {
+        this();
     }
+}
